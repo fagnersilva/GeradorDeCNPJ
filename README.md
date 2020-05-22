fb552f7a1fd5ed89799c7ba24233e7f069118b17
lParanhos <leandro.paranhos@dextra-sw.com>
2020-05-22 09:09:31 -0300

    add Readme

---
+++ README.md
@@ -0,0 +1,35 @@
+# Gerador de CPNJ
+
+Um gerador simples e prático desenvolvido em Golang
+
+## Como começar
+
+É necessário fazer um clone do projeto.
+
+### Pre-requisitos
+
+Ter o Go instalado
+
+```
+https://golang.org/
+```
+
+### Rodando o projeto
+
+Rodar o comando abaixo
+
+
+```
+ PORT=5000 ./bin/geradorCnpj 
+```
+## Visualização
+![Alt Text](./public/assets/preview.gif)
+
+## Authors
+
+* **Leandro Paranhos** - *Dev* - [Github](https://github.com/lParanhos)
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- public/script.js
+++ public/script.js
@@ -1,14 +1,14 @@
- async function fetchData(){
- let response = await  fetch("http://localhost:3000/cnpj")
-   let { data } = await response.json()
-   return data 
+async function fetchData() {
+    let response = await fetch("http://localhost:5000/cnpj")
+    let { data } = await response.json()
+    return data
 }
 
-function formatCnpj(value){
+function formatCnpj(value) {
     return value.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5")
 }
 
-function copy(e){
+function copy(e) {
     const input = e.target;
     input.select();
     input.setSelectionRange(0, 99999);
@@ -26,19 +26,19 @@ function copy(e){
     });
 }
 
-async function makeList(){
+async function makeList() {
     let element = document.querySelector(".list-box");
     let cnpjs = await fetchData();
     const previousList = document.querySelector(".list");
 
-    if(previousList){
+    if (previousList) {
         previousList.parentNode.removeChild(previousList);
     }
 
     const ul = document.createElement("ul");
     ul.classList.add("list");
 
-    for(let i = 0; i < cnpjs.length; i++){
+    for (let i = 0; i < cnpjs.length; i++) {
         let li = document.createElement("li");
         li.classList.add("list-item");
 
